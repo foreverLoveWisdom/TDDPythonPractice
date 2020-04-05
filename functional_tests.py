ea7797f96b00d0e934a85e7358bd32a5fa84c95a
Tien Do <forever.love.wisdom@gmail.com>
2020-04-06 00:07:10 +0700

    Upgrade django and working on Django unittest

--- functional_tests.py
+++ functional_tests.py
@@ -4,6 +4,7 @@
 from selenium import webdriver
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -17,8 +18,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists.
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
         # She types "buy peacock feathers" into a text box
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
@@ -26,6 +27,7 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         # The page generated a unique URL for her -- There is some explanatory text to that effect.
         # She visits that URL - her to-do list is still there.
+
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
