eb621903e4d8ae3b003f4a186775b5fe2734f971
Tien Do <forever.love.wisdom@gmail.com>
2020-04-15 23:17:13 +0700

    Still fail functional test

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a coole new online to-do app.
         # She goes to check out its homepage
@@ -24,18 +29,17 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         # She types "buy peacock feathers" into a text box
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         # She enters "Use peacock feathres to make a fly"
+
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
-        # The page updates again, and now shows both items on her list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathres' for row in rows), "New to-do item did not appear in table")
+        time.sleep(3)
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         # The page generated a unique URL for her -- There is some explanatory text to that effect.
         # She visits that URL - her to-do list is still there.
         # self.fail('Finish the test!')
--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,6 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-
 from lists.views import home_page
 
 
@@ -11,4 +10,13 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
 
         html = response.content.decode('utf8')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'Buy peacock feathers'})
+        self.assertIn('Buy peacock feathers', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
